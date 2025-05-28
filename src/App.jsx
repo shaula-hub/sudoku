b863ca728c76fb6e60ab59632a688d87a5c3f24a
shaula-hub <dagetil04@gmail.com>
2025-05-29 01:09:41 +0300

    Style corrections 4

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function AppContent() {
           style={
             isMobile
               ? {
-                  fontSize: "1rem" /* Like new-game-button */,
+                  fontSize: "2rem" /* Like new-game-button */,
                   fontWeight: "bold",
                   padding: "12px 20px",
                   border: `2px solid ${currentDifficultyColor}`,
